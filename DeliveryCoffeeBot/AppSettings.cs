611433981819d85a89f7ebef841cd258cb7587a9
stager050522 <70406508+Anton050522@users.noreply.github.com>
2022-07-07 12:25:41 +0300

    DonerBot
    DonerBot

--- DeliveryCoffeeBot/AppSettings.cs
+++ DeliveryCoffeeBot/AppSettings.cs
@@ -3,7 +3,7 @@
     public static class AppSettings
     {
         public static string Url { get; set; }
-        public static string Name { get; set; } = "filmgurubot";
-        public static string Key { get; set; } = "5578460043:AAFIeuzXQm4v6e1n6w_Gw5cWc9MB7SMaxOw";
+        public static string Name { get; set; } = "wefwefwefwefwefw_bot";
+        public static string Key { get; set; } = "5586306281:AAEEt1tk0dLu2pRCUZLK8pJfb22Bg4r5mVc";
     }
 }
---
+++ DeliveryCoffeeBot/Doner/DonerTimeCommand.cs
@@ -0,0 +1,43 @@
+﻿using DeliveryCoffeeBot.Doner;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Telegram.Bot;
+using Telegram.Bot.Types;
+
+namespace DeliveryCoffeeBot.Doner
+{
+    public class DonerTimeCommand : Command
+    {
+        public override string Name => "donertime";
+
+        public override async void Execute(Message message, TelegramBotClient client)
+        {
+            var chatId = message.Chat.Id;
+
+
+            if (!ChatDonerParticipants.Participants.ContainsKey(chatId))
+            {
+                ChatDonerParticipants.Participants.Add(chatId, new DonerParticipants());
+            }
+            else if (ChatDonerParticipants.Participants[chatId].Date.Year == DateTime.Now.Year
+                && ChatDonerParticipants.Participants[chatId].Date.Month == DateTime.Now.Month
+                && ChatDonerParticipants.Participants[chatId].Date.Day == DateTime.Now.Day)
+            {
+                await client.SendTextMessageAsync(chatId, "Извините, сегодня команда уже использовалась, попробуйте завтра!");
+
+                return;
+            }
+            else
+            {
+                ChatDonerParticipants.Participants[chatId].Participants = new List<Participant>();
+                ChatDonerParticipants.Participants[chatId].Date = DateTime.Now;
+                ChatDonerParticipants.Participants[chatId].IsUsed = false;
+            }
+
+            await client.SendTextMessageAsync(chatId, "Хорошо! Если вы решили поесть шавы, нажмите на команду '/wantdoner'.");
+        }
+    }
+}
